02b0e8917820045810cbc10763b811a35fe9c293
YapCaiFu <smallmoney.yap@hotmail.com>
2024-07-26 03:42:23 +0800

    (build): build on ios platform

--- ios/WeatherApp/AppDelegate.mm
+++ ios/WeatherApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDAue_7tLW2COAs2qautyPNl37mXt-p36E"];
   self.moduleName = @"WeatherApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
