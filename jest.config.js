c557569211b645def508053b1de5b712c014364e
YapCaiFu <smallmoney.yap@hotmail.com>
2024-07-26 03:26:15 +0800

    (build): init Project

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
+// jest.config.js
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-maps)/)',
+  ],
 };
