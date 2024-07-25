d48ae950ec26d026063c0b62ea132c8c3aa471d8
YapCaiFu <smallmoney.yap@hotmail.com>
2024-07-26 03:35:04 +0800

    (style) add images icon and font style

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,25 @@
+const icMap = require('./icMap.png');
+const icMapActive = require('./icMapActive.png');
+const icSearch = require('./icSearch.png');
+const icSearchActive = require('./icSearchActive.png');
+const icBookmark = require('./icBookmark.png');
+const icBookmarkActive = require('./icBookmarkActive.png');
+const icClose = require('./icClose.png');
+const icUp = require('./icUp.png');
+const icDown = require('./icDown.png');
+const icRefresh = require('./icRefresh.png');
+const icBookmarkMap = require('./icBookmarkMap.png');
+
+export {
+  icMap,
+  icMapActive,
+  icSearch,
+  icSearchActive,
+  icBookmark,
+  icBookmarkActive,
+  icClose,
+  icUp,
+  icDown,
+  icRefresh,
+  icBookmarkMap,
+};
